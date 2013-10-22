892171fa77a6998601b1acce92e55b195d35cb2a
Ian Dees <ian.dees@gmail.com>
2013-10-22 20:24:37 +0000

    Use unicode from psycopg2, shorten date attribute names, deal with anonymous notes.

--- dump.py
+++ dump.py
@@ -1,6 +1,9 @@
 from lxml import etree
 import psycopg2
+import psycopg2.extensions
 import argparse
+
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
 
 parser = argparse.ArgumentParser(description='Exports OSM Notes from the database en masse.')
 parser.add_argument('output_file', help='File to write the resulting XML to.')
@@ -21,24 +24,33 @@ conn = psycopg2.connect(host=args.host, port=args.port, user=args.user, password
 note_cursor = conn.cursor()
 comment_cursor = conn.cursor()
 
-note_cursor.execute("SELECT id,latitude,longitude,created_at,status,closed_at FROM notes WHERE status != 'hidden' AND updated_at > %s", [args.since])
+note_cursor.execute("""SELECT id,latitude,longitude,created_at,status,closed_at
+                       FROM notes
+                       WHERE status != 'hidden' AND updated_at > %s""", [args.since])
 for note in note_cursor:
     note_elem = etree.Element("note", {'id': str(note[0]),
                                        'lat': '%0.7f' % (note[1] / 10000000.),
                                        'lon': '%0.7f' % (note[2] / 10000000.),
                                        'status': note[4],
-                                       'date_created': note[3].replace(microsecond=0).isoformat() + 'Z'})
+                                       'created': note[3].replace(microsecond=0).isoformat() + 'Z'})
 
     if note[4] == 'closed':
-        note_elem.attrib['date_closed'] = note[5].replace(microsecond=0).isoformat() + 'Z'
+        note_elem.attrib['closed'] = note[5].replace(microsecond=0).isoformat() + 'Z'
 
-    comment_cursor.execute("SELECT created_at,author_id,users.display_name,body,event FROM note_comments JOIN users ON (note_comments.author_id=users.id) WHERE note_id = %s", [note[0]])
+    comment_cursor.execute("""SELECT created_at,author_id,users.display_name,body,event
+                              FROM note_comments
+                              FULL OUTER JOIN users ON (note_comments.author_id=users.id)
+                              WHERE note_id = %s ORDER BY created_at""", [note[0]])
     for comment in comment_cursor:
-        comment_elem = etree.SubElement(note_elem, "comment", {'date': comment[0].replace(microsecond=0).isoformat() + 'Z',
-                                                               'uid': str(comment[1]),
-                                                               'user': comment[2],
+        comment_elem = etree.SubElement(note_elem, "comment", {'created': comment[0].replace(microsecond=0).isoformat() + 'Z',
                                                                'action': comment[4]})
-        comment_elem.text = unicode(comment[3], 'utf8')
+
+        if comment[1]:
+            comment_elem.attrib['uid'] = str(comment[1])
+            comment_elem.attrib['user'] = comment[2]
+
+        if comment[3] is not None:
+            comment_elem.text = comment[3]
         
     outfile.write(etree.tostring(note_elem, encoding='utf8', pretty_print=True))
 
